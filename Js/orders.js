949a6affb83c5de4d02cb1f83650321b3371554b
Marcos Paulo <marcospaulo3534q@gmail.com>
2023-01-12 15:06:05 -0300

    Css/Js

---
+++ Js/orders.js
@@ -0,0 +1,34 @@
+const Orders =[
+    {
+    productName: 'Product 1',
+    productNumber: '78213798',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+},
+{
+    productName: 'Product 2',
+    productNumber: '782137898',
+    paymentStatus: 'Due',
+    shipping: 'Declained',
+},
+{
+    productName: 'Product 3',
+    productNumber: '782136798',
+    paymentStatus: 'Due',
+    shipping: 'Declained',
+},
+{
+    productName: 'Product 4',
+    productNumber: '782123798',
+    paymentStatus: 'Due',
+    shipping: 'Delivered',
+},
+{
+    productName: 'Product 5',
+    productNumber: '782133798',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+},
+] 
+   
+
